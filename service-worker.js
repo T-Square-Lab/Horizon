84ed2705ddb9fa72313c7d33a4eed979430fe2cb
T-Square-Lab <316244826+T-Square-Lab@users.noreply.github.com>
2026-08-12 21:35:35 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'horizon-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/Horizon/',
+  '/Horizon/index.html',
+  '/Horizon/logo-horizon.png',
+  '/Horizon/manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
